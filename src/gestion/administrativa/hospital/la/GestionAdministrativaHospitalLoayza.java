6b80503eef5015549f8239a367a07852a72600cb
Dekszx <alexisherencia25@gmail.com>
2025-05-18 11:42:49 -0500

    Login

--- src/gestion/administrativa/hospital/la/GestionAdministrativaHospitalLoayza.java
+++ src/gestion/administrativa/hospital/la/GestionAdministrativaHospitalLoayza.java
@@ -14,7 +14,7 @@ public class GestionAdministrativaHospitalLoayza {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        print();
+        int xd = 23;
     }
     
 }
