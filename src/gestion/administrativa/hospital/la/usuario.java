f54855ade01ca1f789994a41083715013972c46e
Corcho30 <prierwarrior.14@gmail.com>
2025-05-25 12:42:54 -0500

    COMPLETE login
    Se completo el login del proyecto

---
+++ src/gestion/administrativa/hospital/la/usuario.java
@@ -0,0 +1,29 @@
+package login;
+
+public class usuario {
+    private String nombreUsuario;
+    private String contraseña;
+
+    // Constructor
+    public usuario(String nombreUsuario, String contraseña) {
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+    }
+
+    // Getters y Setters
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+}
