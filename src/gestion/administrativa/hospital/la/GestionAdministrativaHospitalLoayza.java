f51aa40a8f753d8a77fa782ca6da71d0617f6b17
Dekszx <alexisherencia25@gmail.com>
2025-05-25 10:34:48 -0500

    Menu terminado

--- src/gestion/administrativa/hospital/la/GestionAdministrativaHospitalLoayza.java
+++ src/gestion/administrativa/hospital/la/GestionAdministrativaHospitalLoayza.java
@@ -3,18 +3,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package gestion.administrativa.hospital.la;
-
 /**
  *
  * @author Alexis
  */
 public class GestionAdministrativaHospitalLoayza {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
 }
